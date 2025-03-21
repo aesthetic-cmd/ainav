47061d195351f85ba17c87ca3cb7c0f7e4896149
aesthetic-cmd <148448022@qq.com>
2025-03-21 15:33:57 +0800

    seven commit

--- next.config.mjs
+++ next.config.mjs
@@ -40,7 +40,8 @@ const nextConfig = {
       "otter.ai",
       "www.dropbox.com"
     ],
-    sizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 128, 256, 384, 512],
     remotePatterns: [
       {
         protocol: "https",
